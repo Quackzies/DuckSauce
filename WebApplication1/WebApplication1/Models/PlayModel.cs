924881ccb00e54ee3c1c266c1a83f95c85e144ae
extfabianjc <38906214+extfabianjc@users.noreply.github.com>
2018-05-04 13:15:12 +0200

    New Repository

---
+++ WebApplication1/WebApplication1/Models/PlayModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class PlayModel
+    {
+        [Key]
+        int KeyId { get; set; }
+        string ErrandTitle { get; set; }
+        string Status { get; set; }
+        string OrderedByCompany { get; set; }
+        string OrderedByPerson { get; set; }
+        string Overseer { get; set; }
+        string Description { get; set; }
+        DateTime Started { get; set; }
+        DateTime Closed { get; set; }
+        DateTime ExpectedClosingDate { get; set; }
+        DateTime Altered { get; set; }
+        DateTime Created { get; set; }
+        string AlteredBy { get; set; }
+        string CreatedBy { get; set; }
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/PlayViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class PlayViewModel
+    {
+        public PlayViewModel(string Errand)
+        {
+            this.ErrandTitle = "Bibliotek";
+        }
+        
+
+        [Key]
+        public int KeyId { get; set; }
+        public string ErrandTitle { get; set; }
+        public string Status { get; set; }
+        public string OrderedByCompany { get; set; }
+        public string OrderedByPerson { get; set; }
+        public string Overseer { get; set; }
+        public string Description { get; set; }
+        public DateTime Started { get; set; }
+        public DateTime Closed { get; set; }
+        public DateTime ExpectedClosingDate { get; set; }
+        public DateTime Altered { get; set; }
+        public DateTime Created { get; set; }
+    }
+}
